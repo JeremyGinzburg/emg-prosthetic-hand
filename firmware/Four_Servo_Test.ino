c3d9942f1a54186bf1d8fce372a25447ee255836
JeremyGinzburg <ginzburgjeremy77@gmail.com>
2026-07-13 11:05:54 -0400

    Add files via upload

---
+++ firmware/Four_Servo_Test.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+int minAngle = 75;
+int maxAngle = 130;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(9);
+  servo2.attach(7);
+  servo3.attach(4);
+  servo4.attach(11);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = minAngle; i <= maxAngle; i++) {
+    servo1.write(i);
+    servo2.write(i);
+    servo3.write(i);
+    servo4.write(i);
+    delay(15);
+  }
+  for (int j = maxAngle; j >= minAngle; j--) {
+    servo1.write(j);
+    servo2.write(j);
+    servo3.write(j);
+    servo4.write(j);
+    delay(15);
+  }
+
+}
